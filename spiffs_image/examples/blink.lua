a95c46d8d8f734a95f3ad07e1ec0ac1608696b0a
jolivepetrus <jolive@iberoxarxa.com>
2016-11-04 16:07:23 +0100

    Add blink examples (blocks and Lua) to spiffs image

--- spiffs_image/examples/blink.lua
+++ spiffs_image/examples/blink.lua
@@ -1,10 +1,10 @@
-led = pio.GPIO12
-
-pio.pin.setdir(pio.OUTPUT, led)
-
-while true do
-  pio.pin.sethigh(led)
-  tmr.delayms(500)
-  pio.pin.setlow(led)
-  tmr.delayms(500)
-end
+thread.start(function()
+  pio.pin.setdir(pio.OUTPUT, pio.GPIO14)
+  pio.pin.setpull(pio.NOPULL, pio.GPIO14)
+  while true do
+    pio.pin.setval(1, pio.GPIO14)
+    tmr.delayms(100)
+    pio.pin.setval(0, pio.GPIO14)
+    tmr.delayms(100)
+  end
+end)
