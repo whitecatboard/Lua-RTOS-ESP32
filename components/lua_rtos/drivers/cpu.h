whitecatboard/Lua-RTOS-ESP32

commit 0a109708474aa32ff0d7d4e8f7d44a64329fe1c4

Pin maps in drivers/cpu.h
* GPIO according to ESP-WROOM-32 pinout
* Add 4 definitions for i2c bit bang version

components/lua_rtos/drivers/cpu.h
@@ -38,113 +38,109 @@
  * ----------------------------------------------------------------
 */
 
-// ESP32 pin constants
-#define PIN_GPIO36  5
-#define PIN_GPIO37  6
-#define PIN_GPIO38  7
-#define PIN_GPIO39  8
-#define PIN_GPIO34 10
-#define PIN_GPIO35 11
-#define PIN_GPIO32 12
-#define PIN_GPIO33 13
-#define PIN_GPIO25 14
-#define PIN_GPIO26 15
-#define PIN_GPIO27 16
-#define PIN_GPIO14 17
-#define PIN_GPIO12 18
-#define PIN_GPIO13 20
-#define PIN_GPIO15 21
-#define PIN_GPIO2  22
-#define PIN_GPIO0  23
-#define PIN_GPIO4  24
-#define PIN_GPIO16 25
-#define PIN_GPIO17 27
-#define PIN_GPIO9  28
-#define PIN_GPIO10 29
-#define PIN_GPIO11 30
-#define PIN_GPIO6  31
-#define PIN_GPIO7  32
-#define PIN_GPIO8  33
-#define PIN_GPIO5  34
-#define PIN_GPIO18 35
-#define PIN_GPIO23 36
-#define PIN_GPIO19 38
-#define PIN_GPIO22 39
-#define PIN_GPIO3  40
-#define PIN_GPIO1  41
-#define PIN_GPIO21 42
+// ESP-WROOM-32 pin constants
+#define PIN_GPIO36	4
+#define PIN_GPIO39	5
+#define PIN_GPIO34	6
+#define PIN_GPIO35	7
+#define PIN_GPIO32	8
+#define PIN_GPIO33	9
+#define PIN_GPIO25	10
+#define PIN_GPIO26	11
+#define PIN_GPIO27	12
+#define PIN_GPIO14	13
+#define PIN_GPIO12	14
+#define PIN_GPIO13	16
+#define PIN_GPIO9	17
+#define PIN_GPIO10	18
+#define PIN_GPIO11	19
+#define PIN_GPIO6	20
+#define PIN_GPIO7	21
+#define PIN_GPIO8	22
+#define PIN_GPIO15	23
+#define PIN_GPIO2	24
+#define PIN_GPIO0	25
+#define PIN_GPIO4	26
+#define PIN_GPIO16	27
+#define PIN_GPIO17	28
+#define PIN_GPIO5	29
+#define PIN_GPIO18	30
+#define PIN_GPIO19	31
+#define PIN_GPIO21	33
+#define PIN_GPIO3	34
+#define PIN_GPIO1	35
+#define PIN_GPIO22	36
+#define PIN_GPIO23	37
 
-// ESP32 available GPIO pins
-#define GPIO0  0
-#define GPIO1  1
-#define GPIO2  2
-#define GPIO3  3
-#define GPIO4  4
-#define GPIO5  5
-#define GPIO6  6
-#define GPIO7  7
-#define GPIO8  8
-#define GPIO9  9
-#define GPIO10 10
-#define GPIO11 11
-#define GPIO12 12
-#define GPIO13 13
-#define GPIO14 14
-#define GPIO15 15
-#define GPIO16 16
-#define GPIO17 17
-#define GPIO18 18
-#define GPIO19 19
-#define GPIO21 21
-#define GPIO22 22
-#define GPIO23 23
-#define GPIO25 25
-#define GPIO26 26
-#define GPIO27 27
-#define GPIO32 32
-#define GPIO33 33
-#define GPIO34 34
-#define GPIO35 35
-#define GPIO36 36
-#define GPIO37 37
-#define GPIO38 38
-#define GPIO39 39
 
-// ESP32 available pin names
-#define GPIO0_NAME  "GPIO0"
-#define GPIO1_NAME  "GPIO1"
-#define GPIO2_NAME  "GPIO2"
-#define GPIO3_NAME  "GPIO3"
-#define GPIO4_NAME  "GPIO4"
-#define GPIO5_NAME  "GPIO5"
-#define GPIO6_NAME  "GPIO6"
-#define GPIO7_NAME  "GPIO7"
-#define GPIO8_NAME  "GPIO8"
-#define GPIO9_NAME  "GPIO9"
-#define GPIO10_NAME "GPIO10"
-#define GPIO11_NAME "GPIO11"
-#define GPIO12_NAME "GPIO12"
-#define GPIO13_NAME "GPIO13"
-#define GPIO14_NAME "GPIO14"
-#define GPIO15_NAME "GPIO15"
-#define GPIO16_NAME "GPIO16"
-#define GPIO17_NAME "GPIO17"
-#define GPIO18_NAME "GPIO18"
-#define GPIO19_NAME "GPIO19"
-#define GPIO21_NAME "GPIO21"
-#define GPIO22_NAME "GPIO22"
-#define GPIO23_NAME "GPIO23"
-#define GPIO25_NAME "GPIO25"
-#define GPIO26_NAME "GPIO26"
-#define GPIO27_NAME "GPIO27"
-#define GPIO32_NAME "GPIO32"
-#define GPIO33_NAME "GPIO33"
-#define GPIO34_NAME "GPIO34"
-#define GPIO35_NAME "GPIO35"
-#define GPIO36_NAME "GPIO36"
-#define GPIO37_NAME "GPIO37"
-#define GPIO38_NAME "GPIO38"
-#define GPIO39_NAME "GPIO39"
+// ESP-WROOM-32 available GPIO pins
+#define GPIO36	4
+#define GPIO39	5
+#define GPIO34	6
+#define GPIO35	7
+#define GPIO32	8
+#define GPIO33	9
+#define GPIO25	10
+#define GPIO26	11
+#define GPIO27	12
+#define GPIO14	13
+#define GPIO12	14
+#define GPIO13	16
+#define GPIO9	17
+#define GPIO10	18
+#define GPIO11	19
+#define GPIO6	20
+#define GPIO7	21
+#define GPIO8	22
+#define GPIO15	23
+#define GPIO2	24
+#define GPIO0	25
+#define GPIO4	26
+#define GPIO16	27
+#define GPIO17	28
+#define GPIO5	29
+#define GPIO18	30
+#define GPIO19	31
+#define GPIO21	33
+#define GPIO3	34
+#define GPIO1	35
+#define GPIO22	36
+#define GPIO23	37
+
+
+// ESP-WROOM-32 available pin names
+#define GPIO36_NAME	"GPIO36"
+#define GPIO39_NAME	"GPIO39"
+#define GPIO34_NAME	"GPIO34"
+#define GPIO35_NAME	"GPIO35"
+#define GPIO32_NAME	"GPIO32"
+#define GPIO33_NAME	"GPIO33"
+#define GPIO25_NAME	"GPIO25"
+#define GPIO26_NAME	"GPIO26"
+#define GPIO27_NAME	"GPIO27"
+#define GPIO14_NAME	"GPIO14"
+#define GPIO12_NAME	"GPIO12"
+#define GPIO13_NAME	"GPIO13"
+#define GPIO9_NAME	"GPIO9"
+#define GPIO10_NAME	"GPIO10"
+#define GPIO11_NAME	"GPIO11"
+#define GPIO6_NAME	"GPIO6"
+#define GPIO7_NAME	"GPIO7"
+#define GPIO8_NAME	"GPIO8"
+#define GPIO15_NAME	"GPIO15"
+#define GPIO2_NAME	"GPIO2"
+#define GPIO0_NAME	"GPIO0"
+#define GPIO4_NAME	"GPIO4"
+#define GPIO16_NAME	"GPIO16"
+#define GPIO17_NAME	"GPIO17"
+#define GPIO5_NAME	"GPIO5"
+#define GPIO18_NAME	"GPIO18"
+#define GPIO19_NAME	"GPIO19"
+#define GPIO21_NAME	"GPIO21"
+#define GPIO3_NAME	"GPIO3"
+#define GPIO1_NAME	"GPIO1"
+#define GPIO22_NAME	"GPIO22"
+#define GPIO23_NAME	"GPIO23"
 
 // ESP32 has only 1 GPIO port
 #define GPIO_PORTS 1
@@ -154,7 +150,7 @@
 
 // ESP32 needs 64 bits for port mask
 typedef uint64_t gpio_port_mask_t;
-#define GPIO_ALL 0b111111110000111011101111111111111111111UL
+#define GPIO_ALL 0b0001111100001110111011111111111111111111UL
 
 #define CPU_FIRST_GPIO GPIO0
 #define CPU_LAST_GPIO  GPIO39
@@ -209,13 +205,16 @@ typedef uint64_t gpio_port_mask_t;
 
 // Number of I2C units (hardware / software)
 #define NI2CHW 0
-#define NI2CBB 1
+#define NI2CBB 4
 
 // ESP32 available bit bang i2c ids
-#define I2CBB1 1
+#define I2CBB1 4
 
 // ESP32 available bit bang i2c names
 #define I2CBB1_NAME  "I2CBB1"
+#define I2CBB2_NAME  "I2CBB2"
+#define I2CBB3_NAME  "I2CBB3"
+#define I2CBB4_NAME  "I2CBB4"
 
 #endif
 

commit c7d768e661f2b33b1bc2d4437f4c11e3d8001997

Resolved bug introduced by commit https://github.com/whitecatboard/Lua-RTOS-ESP32/commit/0a109708474aa32ff0d7d4e8f7d44a64329fe1c4. pio.GPIOxx constants in Lua pio module where not correspond to GPIO numbers.

components/lua_rtos/drivers/cpu.h
@@ -74,38 +74,38 @@
 
 
 // ESP-WROOM-32 available GPIO pins
-#define GPIO36	4
-#define GPIO39	5
-#define GPIO34	6
-#define GPIO35	7
-#define GPIO32	8
-#define GPIO33	9
-#define GPIO25	10
-#define GPIO26	11
-#define GPIO27	12
-#define GPIO14	13
-#define GPIO12	14
-#define GPIO13	16
-#define GPIO9	17
-#define GPIO10	18
-#define GPIO11	19
-#define GPIO6	20
-#define GPIO7	21
-#define GPIO8	22
-#define GPIO15	23
-#define GPIO2	24
-#define GPIO0	25
-#define GPIO4	26
-#define GPIO16	27
-#define GPIO17	28
-#define GPIO5	29
-#define GPIO18	30
-#define GPIO19	31
-#define GPIO21	33
-#define GPIO3	34
-#define GPIO1	35
-#define GPIO22	36
-#define GPIO23	37
+#define GPIO36	36
+#define GPIO39	39
+#define GPIO34	34
+#define GPIO35	35
+#define GPIO32	32
+#define GPIO33	33
+#define GPIO25	25
+#define GPIO26	26
+#define GPIO27	27
+#define GPIO14	14
+#define GPIO12	12
+#define GPIO13	13
+#define GPIO9	9
+#define GPIO10	10
+#define GPIO11	11
+#define GPIO6	6
+#define GPIO7	7
+#define GPIO8	8
+#define GPIO15	15
+#define GPIO2	2
+#define GPIO0	0
+#define GPIO4	4
+#define GPIO16	16
+#define GPIO17	17
+#define GPIO5	5
+#define GPIO18	18
+#define GPIO19	19
+#define GPIO21	21
+#define GPIO3	3
+#define GPIO1	1
+#define GPIO22	22
+#define GPIO23	23
 
 
 // ESP-WROOM-32 available pin names