b59fc0f0b61af46990ef4e6cd1e177dd19146606
Freedom <freelancer.as@gmail.com>
2017-04-25 19:47:13 +0800

    ...

--- components/lua_rtos/Lua/modules/adxl345.c
+++ components/lua_rtos/Lua/modules/adxl345.c
@@ -14,13 +14,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const uint32_t adxl345_i2c_id = 0;
 static const uint8_t adxl345_i2c_addr = 0x53;
 
-static int adxl345_init(lua_State* L) {
+typedef struct {
+	int unit;
+	int transaction;
+} adxl345_user_data_t;
 
-    uint8_t devid;
-    int tran = I2C_TRANSACTION_INITIALIZER;
+static int adxl345_init(lua_State* L) {
 
     int id = luaL_checkinteger(L, 1);
     int mode = luaL_checkinteger(L, 2);
@@ -28,20 +29,49 @@ static int adxl345_init(lua_State* L) {
     int sda = luaL_checkinteger(L, 4);
     int scl = luaL_checkinteger(L, 5);
 
-    i2c_setup(adxl345_i2c_id, mode, speed, sda, scl, 0, 0);
-    // Enable sensor
-    i2c_start(adxl345_i2c_id , &tran);
-    i2c_write_address(adxl345_i2c_id, &tran , adxl345_i2c_addr, false);
-    i2c_write(adxl345_i2c_id , &tran , 0x2D , sizeof(uint8_t));
-    i2c_write(adxl345_i2c_id , &tran , 0x08 , sizeof(uint8_t));
-    i2c_stop(adxl345_i2c_id , &tran);
-    lua_pushinteger(L, 0);
+    if ((error = i2c_setup(id, mode, speed, sda, scl, 0, 0))) {
+    	return luaL_driver_error(L, error);
+    }
+
+    driver_error_t *error;
+
+    // Allocate userdata
+    adxl345_user_data_t *user_data = (adxl345_user_data_t *)lua_newuserdata(L, sizeof(adxl345_user_data_t));
+    if (!user_data) {
+       	return luaL_exception(L, I2C_ERR_NOT_ENOUGH_MEMORY);
+    }
+
+    user_data->unit = id;
+    user_data->transaction = I2C_TRANSACTION_INITIALIZER;
+
+    if ((error = i2c_start(user_data->unit, &user_data->transaction))) {
+    	return luaL_driver_error(L, error);
+    }
+
+	if ((error = i2c_write_address(user_data->unit, &user_data->transaction, adxl345_i2c_addr, false))) {
+    	return luaL_driver_error(L, error);
+    }
+
+    if ((error = i2c_write(user_data->unit, &user_data->transaction, 0x2D , sizeof(uint8_t)))) {
+    	return luaL_driver_error(L, error);
+    }
+    if ((error = i2c_write(user_data->unit, &user_data->transaction, 0x08 , sizeof(uint8_t)))) {
+    	return luaL_driver_error(L, error);
+    }
+
+    if ((error = i2c_stop(user_data->unit, &user_data->transaction))) {
+    	return luaL_driver_error(L, error);
+    }
+
+    luaL_getmetatable(L, "adxl345.trans");
+    lua_setmetatable(L, -2);
+
     return 1;
 }
 
 static int adxl345_read(lua_State* L) {
 
-    uint8_t data[6];
+    char data[6];
     int x,y,z;
     uint8_t start_addr = 0x32;
     int tran = I2C_TRANSACTION_INITIALIZER;
@@ -75,7 +105,7 @@ static const LUA_REG_TYPE adxl345_map[] = {
 
 
 LUALIB_API int luaopen_adxl345( lua_State *L ) {
-    luaL_newmetarotable(L,"adxl345", (void *)adxl345_map);
+    luaL_newmetarotable(L,"adxl345.trans", (void *)adxl345_map);
     return 0;
 }
 
