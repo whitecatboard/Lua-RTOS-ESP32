c36d53bf89e3dc6a02bf8f4d936dddf2b1e5e345
Thomas E. Horner <horner@matrix.t-horner.com>
2017-05-16 21:09:45 +0200

    fixed net.stat for wifi ap mode

--- components/lua_rtos/drivers/wifi.c
+++ components/lua_rtos/drivers/wifi.c
@@ -309,12 +309,19 @@ driver_error_t *wifi_stat(ifconfig_t *info) {
 
 	driver_error_t *error;
 
+	uint8_t mode;
+	if ((error = wifi_check_error(esp_wifi_get_mode((wifi_mode_t*)&mode)))) return error;
+
+	uint8_t interface = ESP_IF_WIFI_AP;
+	if (mode == WIFI_MODE_STA)
+		interface = ESP_IF_WIFI_STA;
+
 	// Get WIFI IF info
-	if ((error = wifi_check_error(tcpip_adapter_get_ip_info(ESP_IF_WIFI_STA, &esp_info)))) return error;
+	if ((error = wifi_check_error(tcpip_adapter_get_ip_info(interface, &esp_info)))) return error;
 
 	// Get MAC info
 	if (status_get(STATUS_WIFI_STARTED)) {
-		if ((error = wifi_check_error(esp_wifi_get_mac(ESP_IF_WIFI_STA, mac)))) return error;
+		if ((error = wifi_check_error(esp_wifi_get_mac(interface, mac)))) return error;
 	}
 
 	// Copy info
