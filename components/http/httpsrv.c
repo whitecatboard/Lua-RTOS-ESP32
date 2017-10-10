09042c848d12227c4f201358a855923f236317a8
the0ne <the0ne@github.com>
2017-10-10 09:41:33 +0200

    remove build warnings

--- components/http/httpsrv.c
+++ components/http/httpsrv.c
@@ -916,8 +916,8 @@ int http_start(lua_State* L) {
 		}
 
 		http_secure.port = luaL_optinteger( L, 2, CONFIG_LUA_RTOS_HTTP_SERVER_PORT_SSL );
-		char *certificate = luaL_optstring( L, 3, NULL );
-		char *private_key = luaL_optstring( L, 4, NULL );
+		const char *certificate = luaL_optstring( L, 3, NULL );
+		const char *private_key = luaL_optstring( L, 4, NULL );
 		http_secure.certificate = certificate ? strdup(certificate) : NULL;
 		http_secure.private_key = private_key ? strdup(private_key) : NULL;
 
