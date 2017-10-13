27c8e76badf107d765734d8246cbea235d51d036
jolivepetrus <jolive@iberoxarxa.com>
2017-10-13 14:23:32 +0200

    Added node.js server program for integrate Lua RTOS / LoRaWAN with TTN servers
    Sourcode is in cloud folder.
    
    For run server:
    
    node whitecat.js

---
+++ cloud/settings.js
@@ -0,0 +1,10 @@
+require("./app.js")
+
+// TTN credentials
+TTNSettings = {
+	"broker": "mqtt://eu.thethings.network",
+	"user": "xxx",
+	"password": "xxx",
+	"port": 1883,
+	"callback": TTNCallback
+}
