5744510bd59096be8c6f5568ebb06585f09f0ba1
jolivepetrus <jolive@iberoxarxa.com>
2018-01-25 02:57:51 +0100

    LoRa multichannel gateway/disable debug messages

--- components/lora/gateway/multi_channel/src/loragw_radio.c
+++ components/lora/gateway/multi_channel/src/loragw_radio.c
@@ -40,7 +40,6 @@ Maintainer: Michael Coracin
 /* --- PRIVATE MACROS ------------------------------------------------------- */
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#define DEBUG_REG 1
 #if DEBUG_REG == 1
     #define DEBUG_MSG(str)              fprintf(stderr, str)
     #define DEBUG_PRINTF(fmt, args...)  fprintf(stderr,"%s:%d: "fmt, __FUNCTION__, __LINE__, args)
