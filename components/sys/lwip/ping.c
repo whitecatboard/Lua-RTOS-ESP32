124ef89d4d9936aca7a8d1a62c61ba81e1ba3783
jolivepetrus <jolive@iberoxarxa.com>
2018-04-23 20:18:39 +0200

    bugfix/ping utility/see issue https://github.com/whitecatboard/Lua-RTOS-ESP32/issues/147

--- components/sys/lwip/ping.c
+++ components/sys/lwip/ping.c
@@ -124,128 +124,135 @@ static float var_time = 0;
  * Helper functions
  *
  */
-static void ping_prepare_echo( struct icmp_echo_hdr *iecho, uint16_t len) {
-	size_t i;
-	size_t data_len = len - sizeof(struct icmp_echo_hdr);
+static void ping_prepare_echo(struct icmp_echo_hdr *iecho, uint16_t len) {
+    size_t i;
+    size_t data_len = len - sizeof(struct icmp_echo_hdr);
 
-	ICMPH_TYPE_SET(iecho, ICMP_ECHO);
-	ICMPH_CODE_SET(iecho, 0);
-	iecho->chksum = 0;
-	iecho->id     = PING_ID;
-	iecho->seqno  = htons(++ping_seq_num);
+    ICMPH_TYPE_SET(iecho, ICMP_ECHO);
+    ICMPH_CODE_SET(iecho, 0);
+    iecho->chksum = 0;
+    iecho->id = PING_ID;
+    iecho->seqno = htons(++ping_seq_num);
 
-	/* fill the additional data buffer with some data */
-	for(i = 0; i < data_len; i++) {
-		((char*)iecho)[sizeof(struct icmp_echo_hdr) + i] = (char)i;
-	}
+    /* fill the additional data buffer with some data */
+    for (i = 0; i < data_len; i++) {
+        ((char*) iecho)[sizeof(struct icmp_echo_hdr) + i] = (char) i;
+    }
 
-	iecho->chksum = inet_chksum(iecho, len);
+    iecho->chksum = inet_chksum(iecho, len);
 }
 
 static err_t ping_send(int s, ip4_addr_t *addr, int size) {
-	struct icmp_echo_hdr *iecho;
-	struct sockaddr_in to;
-	size_t ping_size = sizeof(struct icmp_echo_hdr) + size;
-	int err;
+    struct icmp_echo_hdr *iecho;
+    struct sockaddr_in to;
+    size_t ping_size = sizeof(struct icmp_echo_hdr) + size;
+    int err;
 
-	iecho = (struct icmp_echo_hdr *)mem_malloc((mem_size_t)ping_size);
-	if (!iecho) {
-		return ERR_MEM;
-	}
+    iecho = (struct icmp_echo_hdr *) mem_malloc((mem_size_t) ping_size);
+    if (!iecho) {
+        return ERR_MEM;
+    }
 
-	ping_prepare_echo(iecho, (uint16_t)ping_size);
+    ping_prepare_echo(iecho, (uint16_t) ping_size);
 
-	to.sin_len = sizeof(to);
-	to.sin_family = AF_INET;
-	inet_addr_from_ipaddr(&to.sin_addr, addr);
+    to.sin_len = sizeof(to);
+    to.sin_family = AF_INET;
+    inet_addr_from_ipaddr(&to.sin_addr, addr);
 
-	if ((err = sendto(s, iecho, ping_size, 0, (struct sockaddr*)&to, sizeof(to)))) {
-		transmitted++;
-	}
+    if ((err = sendto(s, iecho, ping_size, 0, (struct sockaddr*) &to,
+            sizeof(to)))) {
+        transmitted++;
+    }
 
-	return (err ? ERR_OK : ERR_VAL);
+    return (err ? ERR_OK : ERR_VAL);
 }
 
 static void ping_recv(int s) {
-	char buf[64];
-	int fromlen, len;
-	struct sockaddr_in from;
-	struct ip_hdr *iphdr;
-	struct icmp_echo_hdr *iecho = NULL;
-	char ipa[16];
-	struct timeval begin;
-	struct timeval end;
-	uint64_t micros_begin;
-	uint64_t micros_end;
-	float elapsed;
+    char buf[64];
+    int fromlen, len;
+    struct sockaddr_in from;
+    struct ip_hdr *iphdr;
+    struct icmp_echo_hdr *iecho = NULL;
+    char ipa[16];
+    struct timeval begin;
+    struct timeval end;
+    uint64_t micros_begin;
+    uint64_t micros_end;
+    float elapsed;
 
-	// Register begin time
-	gettimeofday(&begin, NULL);
+    // Register begin time
+    gettimeofday(&begin, NULL);
 
-	// Send
-	while((len = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*)&from, (socklen_t*)&fromlen)) > 0) {
-		if (len >= (int)(sizeof(struct ip_hdr)+sizeof(struct icmp_echo_hdr))) {
-			// Register end time
-			gettimeofday(&end, NULL);
+    // Send
+    fromlen = sizeof(struct sockaddr_in);
 
-			/// Get from IP address
-			ip4_addr_t fromaddr;
-			inet_addr_to_ipaddr(&fromaddr, &from.sin_addr);
+    while ((len = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*) &from,
+            (socklen_t*) &fromlen)) > 0) {
+        if (len
+                >= (int) (sizeof(struct ip_hdr) + sizeof(struct icmp_echo_hdr))) {
+            // Register end time
+            gettimeofday(&end, NULL);
 
-			strcpy(ipa, inet_ntoa(fromaddr));
+            /// Get from IP address
+            ip4_addr_t fromaddr;
+            inet_addr_to_ipaddr(&fromaddr, &from.sin_addr);
 
-			// Get echo
-			iphdr = (struct ip_hdr *)buf;
-			iecho = (struct icmp_echo_hdr *)(buf + (IPH_HL(iphdr) * 4));
+            strcpy(ipa, inet_ntoa(fromaddr));
 
-			// Print ....
-			if ((iecho->id == PING_ID) && (iecho->seqno == htons(ping_seq_num))) {
-				received++;
+            // Get echo
+            iphdr = (struct ip_hdr *) buf;
+            iecho = (struct icmp_echo_hdr *) (buf + (IPH_HL(iphdr) * 4));
 
-				// Get elapsed time in milliseconds
-				micros_begin  = begin.tv_sec * 1000000;
-				micros_begin += begin.tv_usec;
+            // Print ....
+            if ((iecho->id == PING_ID)
+                    && (iecho->seqno == htons(ping_seq_num))) {
+                received++;
 
-				micros_end  = end.tv_sec * 1000000;
-				micros_end += end.tv_usec;
+                // Get elapsed time in milliseconds
+                micros_begin = begin.tv_sec * 1000000;
+                micros_begin += begin.tv_usec;
 
-				elapsed = (float)(micros_end - micros_begin) / (float)1000.0;
+                micros_end = end.tv_sec * 1000000;
+                micros_end += end.tv_usec;
 
-				// Update statistics
-				// Mean and variance are computed in an incremental way
-				if (elapsed < min_time) {
-					min_time = elapsed;
-				}
+                elapsed = (float) (micros_end - micros_begin) / (float) 1000.0;
 
-				if (elapsed > max_time) {
-					max_time = elapsed;
-				}
+                // Update statistics
+                // Mean and variance are computed in an incremental way
+                if (elapsed < min_time) {
+                    min_time = elapsed;
+                }
 
-				last_mean_time = mean_time;
-				mean_time = (((received - 1) * mean_time) + elapsed) / received;
+                if (elapsed > max_time) {
+                    max_time = elapsed;
+                }
 
-				if (received > 1) {
-					var_time = var_time + ((elapsed - last_mean_time) * (elapsed - mean_time));
-				}
+                last_mean_time = mean_time;
+                mean_time = (((received - 1) * mean_time) + elapsed) / received;
 
-				// Print ...
-				printf("%d bytes from %s: icmp_seq=%d time=%.3f ms\r\n", len, ipa,
-					  ntohs(iecho->seqno), elapsed
-				);
+                if (received > 1) {
+                    var_time = var_time
+                            + ((elapsed - last_mean_time)
+                                    * (elapsed - mean_time));
+                }
 
-				return;
-			} else {
-				// TODO
-			}
-		}
-	}
+                // Print ...
+                printf("%d bytes from %s: icmp_seq=%d time=%.3f ms\r\n", len,
+                        ipa, ntohs(iecho->seqno), elapsed);
 
-	if (len < 0) {
-		printf("Request timeout for icmp_seq %d\r\n", ping_seq_num);
-	}
+                return;
+            } else {
+                // TODO
+            }
+        }
+    }
+
+    if (len < 0) {
+        printf("Request timeout for icmp_seq %d\r\n", ping_seq_num);
+    }
 }
 
-static void stop_action (int i) {
+static void stop_action(int i) {
     signal(i, SIG_DFL);
 
     stopped = 1;
@@ -255,91 +262,93 @@ static void stop_action (int i) {
  * Operation functions
  *
  */
-driver_error_t *ping(const char *name, int count, int interval, int size, int timeout) {
-  driver_error_t *error;
-  struct sockaddr_in address;
-  ip4_addr_t ping_target;
-  int s;
+driver_error_t *ping(const char *name, int count, int interval, int size,
+        int timeout) {
+    driver_error_t *error;
+    struct sockaddr_in address;
+    ip4_addr_t ping_target;
+    int s;
 
-  // Get default values if argument are not provided
-  if (count == 0) {
-	  count = PING_DEFAULT_COUNT;
-  }
-
-  if (interval == 0) {
-	  interval = PING_DEFAULT_INTERVAL;
-  }
-
-  if (size == 0) {
-	  size = PING_DEFAULT_SIZE;
-  }
-
-  if (timeout == 0) {
-	  timeout = PING_DEFAULT_TIMEOUT;
-  }
-
-  // Create socket
-  if ((s = socket(AF_INET, SOCK_RAW, IP_PROTO_ICMP)) < 0) {
-	  return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_CREATE_SOCKET,NULL);
-  }
-
-  // Resolve name
-  if ((error = net_lookup(name, 0, &address))) {
-	  return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_BE_RESOLVED,NULL);
-  }
-
-  ping_target.addr = address.sin_addr.s_addr;
-
-  // Setup socket
-  struct timeval tout;
-
-  // Timeout
-  tout.tv_sec = timeout;
-  tout.tv_usec = 0;
-
-  if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tout, sizeof(tout)) < 0) {
-	  closesocket(s);
-	  return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_SETUP_SOCKET,NULL);
-	  return NULL;
-  }
-
-  stopped = 0;
-  transmitted = 0;
-  received = 0;
-  min_time = FLT_MAX;
-  max_time = 0.0;
-  mean_time = 0.0;
-  var_time = 0.0;
-
-  // Register signal for stop ping
-  signal(SIGINT, stop_action);
-
-  // Begin ping ...
-  char ipa[16];
-
-  strcpy(ipa, inet_ntoa(ping_target));
-  printf("PING %s (%s): %d data bytes\r\n", name, ipa, size);
-
-  ping_seq_num = 0;
-
-  while ((ping_seq_num < count) && (!stopped)) {
-    if (ping_send(s, &ping_target, size) == ERR_OK) {
-      ping_recv(s);
+    // Get default values if argument are not provided
+    if (count == 0) {
+        count = PING_DEFAULT_COUNT;
     }
-    delay(interval * 1000);
-  }
 
-  closesocket(s);
+    if (interval == 0) {
+        interval = PING_DEFAULT_INTERVAL;
+    }
 
-  printf("%d packets transmitted, %d packets received, %.1f%% packet loss\r\n",
-		  transmitted,
-		  received,
-		  ((((float)transmitted - (float)received) / (float)transmitted) * 100.0)
-  );
+    if (size == 0) {
+        size = PING_DEFAULT_SIZE;
+    }
 
-  if (received) {
-	  printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\r\n", min_time, mean_time, max_time, sqrt(var_time / received));
-  }
+    if (timeout == 0) {
+        timeout = PING_DEFAULT_TIMEOUT;
+    }
 
-  return NULL;
+    // Create socket
+    if ((s = socket(AF_INET, SOCK_RAW, IP_PROTO_ICMP)) < 0) {
+        return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_CREATE_SOCKET, NULL);
+    }
+
+    // Resolve name
+    if ((error = net_lookup(name, 0, &address))) {
+        return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_BE_RESOLVED, NULL);
+    }
+
+    ping_target.addr = address.sin_addr.s_addr;
+
+    // Setup socket
+    struct timeval tout;
+
+    // Timeout
+    tout.tv_sec = timeout;
+    tout.tv_usec = 0;
+
+    if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tout, sizeof(tout)) < 0) {
+        closesocket(s);
+        return driver_error(NET_DRIVER, NET_ERR_NAME_CANNOT_SETUP_SOCKET, NULL);
+        return NULL;
+    }
+
+    stopped = 0;
+    transmitted = 0;
+    received = 0;
+    min_time = FLT_MAX;
+    max_time = 0.0;
+    mean_time = 0.0;
+    var_time = 0.0;
+
+    // Register signal for stop ping
+    signal(SIGINT, stop_action);
+
+    // Begin ping ...
+    char ipa[16];
+
+    strcpy(ipa, inet_ntoa(ping_target));
+    printf("PING %s (%s): %d data bytes\r\n", name, ipa, size);
+
+    ping_seq_num = 0;
+
+    while ((ping_seq_num < count) && (!stopped)) {
+        if (ping_send(s, &ping_target, size) == ERR_OK) {
+            ping_recv(s);
+        }
+        delay(interval * 1000);
+    }
+
+    closesocket(s);
+
+    printf(
+            "%d packets transmitted, %d packets received, %.1f%% packet loss\r\n",
+            transmitted, received,
+            ((((float) transmitted - (float) received) / (float) transmitted)
+                    * 100.0));
+
+    if (received) {
+        printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\r\n",
+                min_time, mean_time, max_time, sqrt(var_time / received));
+    }
+
+    return NULL;
 }
