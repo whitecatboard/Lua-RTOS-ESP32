96325f19e3561254471eb201262692420892cb7b
Thomas E. Horner <>
2019-03-11 11:22:55 +0100

    added rc-switch module
    see https://github.com/sui77/rc-switch/ for info about the rc-switch
    library.
    
    it basically enables you to control most rc-sockets and radio led strip
    controller receivers
    also you can (learn and) listen to signals of many rc remote controls
    and e.g. act as if you were a standard rc-socket
    
    search for "xkfst 433mhz transmitter reciever module kit" to find
    thousands of offers of the (very) inexpensive hardware used

---
+++ components/rc-switch/rc-switch.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2015 - 2018, IBEROXARXA SERVICIOS INTEGRALES, S.L.
+ * Copyright (C) 2015 - 2019, Thomas E. Horner (whitecatboard.org@horner.it)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *     * The WHITECAT logotype cannot be changed, you can remove it, but you
+ *       cannot change it in any way. The WHITECAT logotype is:
+ *
+ *          /\       /\
+ *         /  \_____/  \
+ *        /_____________\
+ *        W H I T E C A T
+ *
+ *     * Redistributions in binary form must retain all copyright notices printed
+ *       to any local or remote output device. This include any reference to
+ *       Lua RTOS, whitecatboard.org, Lua, and other copyright notices that may
+ *       appear in the future.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sdkconfig.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#if CONFIG_LUA_RTOS_LUA_USE_RCSWITCH
+
+// Module errors
+#define LUA_RCSWITCH_ERR_CANT_START_THREAD       (DRIVER_EXCEPTION_BASE(RCSWITCH_DRIVER_ID) |  0)
+
+extern const int rcswitch_errors;
+extern const int rcswitch_error_map;
+
+int rcswitch_send(lua_State *L);
+int rcswitch_listen(lua_State *L);
+void rcswitch_destroy(lua_State *L);
+
+#endif
