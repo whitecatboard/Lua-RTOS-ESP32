3053c0972c7cb70ae7bcb4959f74f23964eebd21
Jaume Olivé <jolive@whitecatboard.org>
2025-07-22 22:11:06 +0200

    idf-5.2/build system/bugfix/ensure that file system image is always updated when using target makefs

--- cmake/fs.cmake
+++ cmake/fs.cmake
@@ -9,16 +9,6 @@ elseif("${CONFIG_LUA_RTOS_USE_LFS}" STREQUAL "y")
   set(fs_type "lfs")
 endif()
 
-add_custom_command(OUTPUT ${project_path}/build/${fs_type}_image.img
-  COMMAND ${project_path}/mkspiffs/src/mkspiffs.exe 
-          -c ${project_path}/build/tmp-fs
-          -b ${CONFIG_LUA_RTOS_SPIFFS_LOG_BLOCK_SIZE}
-          -p ${CONFIG_LUA_RTOS_SPIFFS_LOG_PAGE_SIZE}
-          -s ${FS_SIZE} ${project_path}/build/spiffs_image.img
-            
-  VERBATIM
-)
-
 add_custom_target(fs-prepare
   COMMAND ${CMAKE_COMMAND}
   		  -DFS_ROOT_PATH=${FS_ROOT_PATH} 
@@ -27,11 +17,23 @@ add_custom_target(fs-prepare
   WORKING_DIRECTORY ${project_path}
   VERBATIM
 )
+
+add_custom_command(OUTPUT ${project_path}/build/${fs_type}_image.img
+  COMMAND ${project_path}/mkspiffs/src/mkspiffs.exe 
+          -c ${project_path}/build/tmp-fs
+          -b ${CONFIG_LUA_RTOS_SPIFFS_LOG_BLOCK_SIZE}
+          -p ${CONFIG_LUA_RTOS_SPIFFS_LOG_PAGE_SIZE}
+          -s ${FS_SIZE} ${project_path}/build/spiffs_image.img
+          
+  COMMAND ${ESPTOOLPY} write_flash ${FS_BASE_ADDR} ${project_path}/build/${fs_type}_image.img
+  
+  VERBATIM
+)
  
 add_custom_target(flashfs
-  COMMAND ${ESPTOOLPY} write_flash ${FS_BASE_ADDR} ${project_path}/build/${fs_type}_image.img
   DEPENDS gen-part
-  DEPENDS fs-prepare
+  DEPENDS fs-prepare             
   DEPENDS ${project_path}/build/${fs_type}_image.img
+  COMMAND ${CMAKE_COMMAND} -E remove ${project_path}/build/${fs_type}_image.img
 )
  
