bc8129a79847868fb2070df0ac6fbb3dc8b09715
Jaume Olivé Petrus <jolive@iberoxarxa.com>
2026-03-27 16:46:54 +0100

    ifg-5.2/bugfix/build error when external GPIO is not used

--- sys/drivers/gpio_debouncing.c
+++ sys/drivers/gpio_debouncing.c
@@ -65,7 +65,11 @@ static uint32_t max_threshold = 0;
 void gpio_isr(void *args) {
     uint8_t pin = ((uint32_t)args);
 
+	#if EXTERNAL_GPIO
     if ((debouncing->mask == 0) && (debouncing->mask_ext == 0)) {
+	#else
+	if (debouncing->mask == 0) {
+	#endif
         // Stop timer
     	tmr_ll_stop(GPIO_DEBOUNCING_TIMER);
     }
